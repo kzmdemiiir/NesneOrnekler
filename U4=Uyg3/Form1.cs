abe908c00dbad2e9325f5b5b072a395db7476972
BIL-LAB1PC10\Ogrenci <123@123.com>
2023-02-28 12:21:11 +0300

    no message

---
+++ U4=Uyg3/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U4_Uyg3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] isimler = new string[5];
+        int[] notlar = new int[5];
+        int index = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (index < isimler.Length)
+            {
+                isimler[index] = textBox1.Text;
+                notlar[index] = int.Parse(textBox2.Text);
+
+                index++;
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+        }
+    }
+}
---
+++ U4_Uyg2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U4_Uyg2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < isimler.length; i++)
+                listBox1.Items.Add(isimler[i]);
+        }
+    }
+}
